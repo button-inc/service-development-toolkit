22ac88ef3c4c8ff5d9b442b0ba5b931199162dbf
Jonathan Langlois <jon@button.is>
2020-11-18 09:22:26 -0800

    style(.eslintrc.js): turn off prefer default export
    Linter wants default import since only 1 element, more will be added later though

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': 0,
     'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
   },
   overrides: [],
 };
