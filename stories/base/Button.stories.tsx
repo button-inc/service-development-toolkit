0e476c0b8a6918535e1c3f804a168e1e57f5bb43
Jonathan Langlois <jon@button.is>
2020-11-26 14:54:58 -0800

    refactor(stories): add story template
    Refactor repetetive story into template

--- stories/base/Button.stories.tsx
+++ stories/base/Button.stories.tsx
@@ -1,47 +1,7 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import storyBuilder from '../templates/ButtonTemplate';
 import Button from '../../packages/component-library/src/Button';
-import { HtmlOnlyWrapper, HtmlWithCssWrapper } from '../helpers';
 
-export default {
-  title: 'Button',
-  component: Button,
-  argTypes: { onClick: { action: 'clicked' } },
-} as Meta;
-
-const Template: Story = args => (
-  <>
-    <h3>HTML Only</h3>
-    <HtmlOnlyWrapper>
-      <Button {...args} >Button</Button>
-    </HtmlOnlyWrapper>
-
-    <h3>HTML + CSS</h3>
-    <HtmlWithCssWrapper>
-      <Button {...args} >Button</Button>
-    </HtmlWithCssWrapper>
-
-    <h3>HTML + CSS + JS</h3>
-    <Button {...args} >Button</Button>
-  </>
-);
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  secondary: true,
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  large: true,
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  small: true,
-};
+const exported = storyBuilder(Button, 'Button');
+const metaData = exported.default;
+export { metaData as default };
+export const { Primary, Secondary, Large, Small } = exported;
--- stories/gov/Button.stories.tsx
+++ stories/gov/Button.stories.tsx
@@ -1,32 +1,7 @@
-import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import storyBuilder from '../templates/ButtonTemplate';
 import Button from '../../packages/component-library-gov/src/Button';
 
-export default {
-  title: 'Button',
-  component: Button,
-} as Meta;
-
-// const Template: Story<ButtonProps> = (args) => <Button {...args} />;
-const Template: Story = args => <Button {...args}>Button</Button>;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  secondary: true,
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  large: true,
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  small: true,
-};
+const exported = storyBuilder(Button, "Button")
+const metaData = exported.default
+export {metaData as default}
+export const {Primary, Secondary, Large, Small} = exported
