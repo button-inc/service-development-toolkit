6e07d233f4232008eccc7e7b5e04ae8f682ea683
Jonathan Langlois <jon@button.is>
2020-12-28 12:52:58 -0800

    feat: add fieldset component
    102

--- examples/nextjs-with-typescript/pages/index.tsx
+++ examples/nextjs-with-typescript/pages/index.tsx
@@ -2,6 +2,32 @@ import Button2 from 'component-library/Button';
 import Checkbox from 'component-library/Checkbox';
 import CheckboxGov from 'component-library-gov/Checkbox';
 import Input from 'component-library-gov/Input';
+import Fieldset from 'component-library-gov/Fieldset';
+import { applyTheme } from 'component-library/Fieldset';
+
+const styles = {
+  shared: {
+  },
+  defaultProps: {
+    size: 'tiny'
+  },
+  tiny: {
+    container: `
+      border: 5px solid gold;
+      background-color: green;
+    `,
+    legend: 'color: purple;',
+  },
+  large: {
+    container: `
+      border: 5px solid red;
+      background: yellow;
+    `,
+    legend: 'color: orange;',
+  },
+};
+
+const FieldsetGov: any = applyTheme(styles);
 
 export default function Home() {
   return (
@@ -9,13 +35,7 @@ export default function Home() {
       <form action="">
         <div>
           {/* Base checkbox has default options */}
-          <Checkbox mini style={{ margin: '20px' }} label="test" />
-          <Checkbox tiny style={{ margin: '20px' }} label="test" />
-          <Checkbox small style={{ margin: '20px' }} label="test" />
-          <Checkbox medium style={{ margin: '20px' }} label="test" />
-          <Checkbox large style={{ margin: '20px' }} label="test" />
-          <Checkbox big style={{ margin: '20px' }} label="test" />
-          <Checkbox huge style={{ margin: '20px' }} label="test" />
+          <Checkbox size='mini' label="test" />
         </div>
         <div style={{ margin: '20px' }}>
           {/* Styled checkbox falls back to tiny style from default props unless prop defined */}
@@ -23,7 +43,7 @@ export default function Home() {
           <CheckboxGov tiny style={{ margin: '20px' }} label="test" />
           <CheckboxGov small style={{ margin: '20px' }} label="test" />
           <CheckboxGov medium style={{ margin: '20px' }} label="test" />
-          <CheckboxGov large style={{ margin: '20px' }} label="test" />
+          <CheckboxGov size={'large'} style={{ margin: '20px' }} label="large" />
           <CheckboxGov big style={{ margin: '20px' }} label="test" />
           <CheckboxGov huge style={{ margin: '20px' }} label="test" />
         </div>
@@ -34,6 +54,11 @@ export default function Home() {
         </div>
         <div>
           <Input />
+        </div>
+        <div>
+          <FieldsetGov title="hihihi" size='large'>
+            {/* <CheckboxGov size={'large'} style={{ margin: '20px' }} label="large" /> */}
+          </FieldsetGov>
         </div>
       </form>
     </>
