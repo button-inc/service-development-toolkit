aab0ee19942908d0b8b104e6c8bfb857d7e33262
Jonathan Langlois <jon@button.is>
2020-12-28 13:31:37 -0800

    test: add stories for fieldset
    adds stories, fixes htmlonly wrapper components regex to remove classes
    
    102

--- stories/helpers.ts
+++ stories/helpers.ts
@@ -6,7 +6,7 @@ const getHtmlWithCss = child => renderToStaticMarkup(child);
 const HtmlWithCssWrapper = ({ children }) => ReactHtmlParser(getHtmlWithCss(children));
 const HtmlOnlyWrapper = ({ children }) => {
   const htmlWithCssString = getHtmlWithCss(children);
-  const htmlString = htmlWithCssString.replace(/class=".+"/, '');
+  const htmlString = htmlWithCssString.replace(/class=".+?"/g, '');
   return ReactHtmlParser(htmlString);
 };
 
