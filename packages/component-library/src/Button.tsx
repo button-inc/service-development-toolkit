bb7b99c0026aa106faf87c7e767013a34c65bdc7
Jonathan Langlois <jon@button.is>
2021-01-04 14:43:03 -0800

    refactor: refactor button
    refactor to use new helpers

--- packages/component-library/src/Button.tsx
+++ packages/component-library/src/Button.tsx
@@ -1,63 +1,43 @@
-import styled from 'styled-components';
+import React from 'react';
 import GlobalStyles from './GlobalStyles';
-import { applyThemeFactory } from './helpers';
+import { getStyleBuilder } from './helpers';
+import { SizeStyles } from './interface/size';
+import { VariantStyles } from './interface/variant';
 
 type ButtonProps = {
-  secondary?: boolean;
-  primary?: boolean;
-  large?: boolean;
+  disabled?: boolean;
+  size?: string;
+  variant?: string;
+  fullHeight?: boolean;
+  fullWidth?: boolean;
+  [propName: string]: any;
 };
 
-interface Styles {
-  shared?: string;
-  primary?: string;
-  secondary?: string;
-  large?: string;
-  small?: string;
-  warning?: string;
-  danger?: string;
-  success?: string;
+interface Styles extends VariantStyles, SizeStyles {
+  defaultProps?: object;
+  shared?: object;
 }
 
 const defaultStyles: Styles = {
-  shared: `
-  color: ${GlobalStyles.fontColor};
-  border-radius: 70px;
-  padding: 25px 30px;
-  line-height: 26px;
-  font-size: 1rem;
-  border:none;
-  `,
-  primary: `
-    color: ${GlobalStyles.fontColor};
-    background-color: ${GlobalStyles.primaryColor};
-    font-family: ${GlobalStyles.primaryFont}, ${GlobalStyles.secondaryFont};
-  `,
-  secondary: `
-    background: blue
-  `,
-  large: `
-    line-height: 39px;
-    font-size: 1.5rem;
-  `,
-  small: `
-    line-height: 13px;
-    font-size: 0.75rem;
-  `,
-  warning: `
-    background-color: ${GlobalStyles.warningColor}
-  `,
-  danger: `
-    background-color: ${GlobalStyles.dangerColor}
-  `,
-  success: `
-    background-color: ${GlobalStyles.successColor}
-  `,
+  shared: {
+    button: `
+      color: ${GlobalStyles.fontColor};
+      border-radius: 70px;
+      padding: 25px 30px;
+      line-height: 26px;
+      font-size: 1rem;
+      border:none;
+    `,
+  },
 };
 
-const BaseButton = styled.button``;
+export const applyTheme = stylesToApply => {
+  const styleBuilder = getStyleBuilder(stylesToApply, ['size', 'variant', 'fullWidth', 'fullHeight']);
+  const Sbutton = styleBuilder('button', 'button');
 
-export const applyTheme = applyThemeFactory<Styles, ButtonProps>(defaultStyles, BaseButton);
+  const BaseComponent = (props: ButtonProps) => <Sbutton {...props} />;
+  return BaseComponent;
+};
 
 const Button = applyTheme(defaultStyles);
 
