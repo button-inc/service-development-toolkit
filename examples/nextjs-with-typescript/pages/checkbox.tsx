f008c13fd0bf0fc7b9558b3367be1004d3bcdb19
Jonathan Langlois <jon@button.is>
2021-02-08 12:40:48 -0800

    refactor(component-library, component-library-gov): update checkbox to use new builder

---
+++ examples/nextjs-with-typescript/pages/checkbox.tsx
@@ -0,0 +1,13 @@
+import Checkbox from 'component-library-gov/Checkbox';
+
+export default function Select() {
+  return (
+    <>
+      <form action="">
+        <Checkbox label="First" size="tiny" variant="secondary" name="test name" />
+        <Checkbox label="Second" size="medium" type="email" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
