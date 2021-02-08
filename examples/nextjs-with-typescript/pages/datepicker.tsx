c84795e529f519c5a9b0575f0848e454dd1c6357
Junmin Ahn <junmin@button.is>
2021-02-08 12:40:52 -0800

    feat(component-library): add datepicker component

---
+++ examples/nextjs-with-typescript/pages/datepicker.tsx
@@ -0,0 +1,15 @@
+import DatePickerGov from 'component-library-gov/DatePicker';
+
+export default function DatePicker() {
+  return (
+    <>
+      <form action="">
+        <DatePickerGov size="tiny" />
+        <hr />
+        <DatePickerGov size="medium" variant="secondary" name="select2" label="Select 2" />
+        <hr />
+        <DatePickerGov label="Select 3" display="block" onChange={console.log} />
+      </form>
+    </>
+  );
+}
