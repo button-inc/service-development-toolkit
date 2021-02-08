88c607a11b66b4baa9e7f39e7963335f14670fac
Jonathan Langlois <jon@button.is>
2021-02-08 12:50:32 -0800

    feat(component-library):
    update button to use new builder

--- packages/component-library-gov/src/Button.tsx
+++ packages/component-library-gov/src/Button.tsx
@@ -1,57 +1,41 @@
 import { applyTheme } from 'component-library/Button';
-import GlobalStyles from './GlobalStyles';
 
 const styles = {
   shared: {
     button: `
-    border-radius: 4px;
-    text-align: center;
-    text-decoration: none;
-    display: block;
-    font-size: 18px;
-    font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif;
-    font-weight: 700;
-    letter-spacing: 1px;
-    cursor: pointer;
-    &:focus {
-      outline: 4px solid #3B99FC;
-      outline-offset: 1px;
-    }
-    &:active {
-      opacity: 1;
-    }
-  `,
+      border-radius: 10px;
+    `,
   },
-  primary: {
-    button: `
-    background-color: ${GlobalStyles.primaryColor};
-    border: none;
-    color: white;
-    padding: 12px 32px;
-
-    &:hover {
-      text-decoration: underline;
-      opacity: 0.80;
-    }
-  `,
+  size: {
+    tiny: {
+      button: 'font-size: 15px;',
+    },
+    medium: {
+      input: 'font-size: 20px;',
+    },
   },
-  secondary: {
-    button: `
-    background: none;
-    border: 2px solid #003366;
-    padding: 10px 30px;
-    color: #003366;
-
-    &:hover {
-      opacity: 0.80;
-      text-decoration: underline;
-      background-color: #003366;
-      color: #FFFFFF;
-    }
-  `,
+  variant: {
+    secondary: {
+      button: `
+                  border-color: blue;
+                `,
+    },
+    warning: {
+      button: `
+                  border-color: red;
+                `,
+    },
   },
 };
 
-const Button = applyTheme(styles);
+const config = {
+  defaultProps: {
+    variant: 'warning',
+    size: 'tiny',
+  },
+  staticProps: ['fullWidth', 'fullHeight'],
+};
 
-export default Button;
+const Select: any = applyTheme(styles, config);
+
+export default Select;
