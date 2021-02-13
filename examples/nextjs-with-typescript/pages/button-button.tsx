5e901cd4e1505a47adf85ebf2326e4c66cee6d72
Junmin Ahn <junmin@button.is>
2021-02-12 16:32:32 -0800

    chore(component-library): add break props in theme config

--- examples/nextjs-with-typescript/pages/button-button.tsx
+++ examples/nextjs-with-typescript/pages/button-button.tsx
@@ -1,24 +1,26 @@
+import React, { useState } from 'react';
 import Button from 'component-library-button/Button';
 
 export default function ButtonPage() {
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <>
-      <Button variant="primary">Primary</Button>&nbsp;
-      <Button variant="secondary">Secondary</Button>&nbsp;
-      <Button variant="warning">Warning</Button>
-      <br />
-      <br />
-      <Button variant="primary" disabled>
+      <Button variant="primary" disabled={disabled}>
         Primary
       </Button>
       &nbsp;
-      <Button variant="secondary" disabled>
+      <Button variant="secondary" disabled={disabled}>
         Secondary
       </Button>
       &nbsp;
-      <Button variant="warning" disabled>
+      <Button variant="warning" disabled={disabled}>
         Warning
       </Button>
+      &nbsp;
+      <button type="button" onClick={() => setDisabled(true)}>
+        Disable Them!
+      </button>
     </>
   );
 }
