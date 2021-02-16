64f7eb0e125ebf23ccd036127e852c433bffe08b
Jonathan Langlois <jon@button.is>
2021-02-16 11:39:55 -0800

    chore([form-schema]): helper function to get url array
    69

--- packages/form-schema/src/helpers.ts
+++ packages/form-schema/src/helpers.ts
@@ -1,4 +1,5 @@
 import forEach from 'lodash/forEach';
+import { ISchema } from './interfaces';
 
 export const getUrlPage = url => {
   const splitUrl = url.split('/');
@@ -12,6 +13,17 @@ export const parseUrl = (getRoute: string, nextPage: string) => {
   if (getRoute[getRoute.length - 1] === '/') route = `${getRoute}${nextPage}`;
   else route = `${getRoute}/${nextPage}`;
   return route;
+};
+
+export const generateUrlArray = (schema: ISchema) => {
+  const propertyEntries = Object.entries(schema.properties);
+  console.log(propertyEntries);
+  const urlArray: string[] = [];
+  propertyEntries.forEach(([_propertyName, propertyValue]) => {
+    if (propertyValue.urlPostfix) urlArray.push(propertyValue.urlPostfix);
+    else urlArray.push('');
+  });
+  return urlArray;
 };
 
 export const removeDefaultLabels = (schema, uiSchema) => {
