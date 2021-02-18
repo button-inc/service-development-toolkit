e1b93dbd1bdb8abd97a3539159ca9d6fd4ddd485
Junmin Ahn <junmin@button.is>
2021-02-18 14:35:22 -0800

    feat(component-library-bcgov): add bcgov Button theme

--- packages/component-library-bcgov/src/Button.tsx
+++ packages/component-library-bcgov/src/Button.tsx
@@ -3,37 +3,157 @@ import { applyTheme } from 'component-library/Button';
 const styles = {
   shared: {
     button: `
-      border-radius: 10px;
+      border-radius: 4px;
+      padding: 0.78571429em 1.6em 0.78571429em;
+      text-align: center;
+      text-decoration: none;
+      Font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif;
+      font-weight: 700;
+      letter-spacing: 1px;
+      display: inline-block;
+      cursor: pointer;
     `,
   },
   size: {
-    tiny: {
-      button: 'font-size: 15px;',
+    small: {
+      button: `
+        font-size: 0.8rem;
+      `,
     },
     medium: {
-      input: 'font-size: 20px;',
+      button: `
+        font-size: 1rem;
+      `,
+    },
+    large: {
+      button: `
+        font-size: 1.2rem;
+      `,
     },
   },
   variant: {
+    primary: {
+      button: `
+        background-color: #003366;
+        border: none;
+        color: #fff;
+
+        &:hover {
+          text-decoration: underline;
+          opacity: 0.80;
+        }
+
+        &:focus {
+          outline: 4px solid #3B99FC;
+          outline-offset: 1px;
+        }
+
+        &:active {
+          opacity: 1;
+        }
+      `,
+    },
+    'primary-inverse': {
+      button: `
+        background-color: #fff;
+        border: none;
+        color: #313132;
+
+        &:hover {
+          text-decoration: underline;
+          background-color: #f2f2f2
+        }
+
+        &:focus {
+          outline: 4px solid #3B99FC;
+          outline-offset: 1px;
+        }
+
+        &:active {
+          background-color: #fff;
+        }
+      `,
+    },
+    'primary-disabled': {
+      button: `
+        background-color: #003366;
+        opacity: 0.3;
+        border: none;
+        color: #fff;
+        cursor: not-allowed;
+      `,
+    },
     secondary: {
       button: `
-                  border-color: blue;
-                `,
+        background: none;
+        border: 2px solid #003366;
+        color: #003366;
+
+        &:hover {
+          opacity: 0.80;
+          text-decoration: underline;
+          background-color: #003366;
+          color: #FFFFFF;
+        }
+
+        &:focus {
+          outline-offset: 1px;
+          outline: 4px solid #3B99FC;
+        }
+
+        &:active {
+          opacity: 1;
+        }
+      `,
     },
-    warning: {
+    'secondary-inverse': {
       button: `
-                  border-color: red;
-                `,
+        background: none;
+        border: 2px solid #fff;
+        color: #fff;
+
+        &:hover {
+          text-decoration: underline;
+          background-color: #fff;
+          color: #313132;
+        }
+
+        &:focus {
+          outline-offset: 1px;
+          outline: 4px solid #3B99FC;
+        }
+
+        &:active {
+          background-color: #f2f2f2;
+          color: #313132;
+        }
+
+        &:disabled {
+          background-color: #003366;
+          opacity: 0.3;
+          cursor: not-allowed;
+        }
+      `,
+    },
+    'secondary-disabled': {
+      button: `
+        background-color: #fff;
+        opacity: 0.3;
+        border: 2px solid #003366;
+        color: #003366;
+        cursor: not-allowed;
+      `,
     },
   },
 };
 
 const config = {
   defaultProps: {
-    variant: 'warning',
-    size: 'tiny',
+    variant: 'primary',
+    size: 'medium',
   },
-  staticProps: ['fullWidth', 'fullHeight'],
+  breakProps: [],
+  staticProps: [],
 };
 
 const Button: any = applyTheme(styles, config);
--- packages/component-library-bcgov/stories/Button.stories.tsx
+++ packages/component-library-bcgov/stories/Button.stories.tsx
@@ -6,11 +6,23 @@ import Button from '../src/Button';
 export default {
   title: 'Button',
   component: Button,
-  argTypes: { onClick: { action: 'clicked' } },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
 } as Meta;
 
 const Template: Story = args => (
-  <>
+  <div
+    style={{
+      backgroundColor: args.variant.endsWith('-inverse') ? '#003366' : '#fff',
+    }}
+  >
     <h3>HTML Only</h3>
     <HtmlOnlyWrapper>
       <Button {...args}>Button</Button>
@@ -23,14 +35,47 @@ const Template: Story = args => (
 
     <h3>HTML + CSS + JS</h3>
     <Button {...args}>Button</Button>
-  </>
+  </div>
 );
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+  size: 'medium',
+  disabled: false,
+};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
+  size: 'medium',
+  disabled: false,
 };
-export const Warning = Template.bind({});
-Warning.args = {
-  variant: 'warning',
+
+export const PrimaryInverse = Template.bind({});
+Primary.args = {
+  variant: 'primary-inverse',
+  size: 'medium',
+  disabled: false,
+};
+
+export const SecondaryInverse = Template.bind({});
+Secondary.args = {
+  variant: 'secondary-inverse',
+  size: 'medium',
+  disabled: false,
+};
+
+export const PrimaryDisabled = Template.bind({});
+Primary.args = {
+  variant: 'primary-disabled',
+  size: 'medium',
+  disabled: true,
+};
+
+export const SecondaryDisabled = Template.bind({});
+Secondary.args = {
+  variant: 'secondary-disabled',
+  size: 'medium',
+  disabled: true,
 };
