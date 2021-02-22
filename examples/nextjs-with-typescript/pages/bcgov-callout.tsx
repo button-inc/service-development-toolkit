770c28b3ede3030c189d78fd9fe974bb7240c033
Junmin Ahn <junmin@button.is>
2021-02-22 12:14:19 -0800

    feat(component-library-bcgov): add bcgov Callout theme

---
+++ examples/nextjs-with-typescript/pages/bcgov-callout.tsx
@@ -0,0 +1,11 @@
+import Callout from 'component-library-bcgov/Callout';
+
+export default function CalloutPage() {
+  return (
+    <>
+      <Callout content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tincidunt risus." />
+      <br />
+      <Callout>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tincidunt risus.</Callout>
+    </>
+  );
+}
---
+++ packages/component-library-bcgov/stories/Callout.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { HtmlOnlyWrapper, HtmlWithCssWrapper } from '../../../stories/helpers';
+import Callout from '../src/Callout';
+
+export default {
+  title: 'Callout',
+  component: Callout,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story = args => (
+  <>
+    <Callout {...args} />
+  </>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+  size: 'medium',
+  content:
+    'By March 1, 2018, eligible residents of B.C. are expected to renew enrolment in the Medical Services Plan (MSP) and get a BC Services Card.',
+};
