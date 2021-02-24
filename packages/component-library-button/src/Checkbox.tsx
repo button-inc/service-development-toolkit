7099eba83c0aa21385267d0db38730393d6af4ac
Junmin Ahn <junmin@button.is>
2021-02-24 13:12:59 -0800

    feat(component-library-button): add button Checkbox theme

---
+++ packages/component-library-button/src/Checkbox.tsx
@@ -0,0 +1,87 @@
+import { applyTheme } from 'component-library/Checkbox';
+import { Check, toSvgUrl } from './fontawesome';
+
+const checkSVG = toSvgUrl(Check);
+
+const styles = {
+  shared: {
+    label: `
+      display: block;
+      position: relative;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      cursor: pointer;
+      height: 1em;
+      line-height: 1em;
+      padding-left: 1.5em;
+    `,
+    input: `
+      position: absolute;
+      opacity: 0;
+      height: 0;
+      width: 0;
+
+      &:checked ~ .checkmark:after {
+        display: block;
+      }
+    `,
+    checkmark: `
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 1em;
+      width: 1em;
+      box-shadow: 0 0 0 0.12em #606060 inset;
+      border-radius: 0.2em;
+
+      &:after {
+        content: ' ';
+        background-image: ${checkSVG};
+        background-size: contain;
+        background-repeat: no-repeat;
+        position: absolute;
+        display: none;
+        width: 0.625em;
+        top: 0.1875em;
+        left: 0.1875em;
+        height: 100%;
+        text-align: center;
+        cursor: pointer;
+        pointer-events: none;
+        -webkit-transition: .25s all ease;
+        -o-transition: .25s all ease;
+        transition: .25s all ease;
+      }
+    `,
+  },
+  size: {
+    small: {
+      label: `
+        font-size: 0.8rem;
+      `,
+    },
+    medium: {
+      label: `
+        font-size: 1rem;
+      `,
+    },
+    large: {
+      label: `
+        font-size: 1.2rem;
+      `,
+    },
+  },
+};
+
+const config = {
+  defaultProps: {
+    size: 'medium',
+  },
+  staticProps: [],
+};
+
+const Checkbox: any = applyTheme(styles, config);
+
+export default Checkbox;
