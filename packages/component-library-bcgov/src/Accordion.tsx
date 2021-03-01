dfd0f053c8be969a26b2eae04084e1c7c1af17fa
Junmin Ahn <junmin@button.is>
2021-03-01 12:24:23 -0800

    feat(component-library): add Accordion themes

---
+++ packages/component-library-bcgov/src/Accordion.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import noop from 'lodash/noop';
+import { applyTheme, StyleConfig } from 'component-library/Card';
+import { Plus, Minus, FaSVG } from './fontawesome';
+
+const styles = {
+  shared: {
+    container: `
+      font-size: 1rem;
+      border-top: 1px solid #000057;
+      border-bottom: 1px solid #000057;
+    `,
+    header: `
+      display: flex;
+      padding: 1em 0;
+    `,
+    content: `
+      min-height: 5em;
+    `,
+    toggle: `
+      cursor: pointer !important;
+    `,
+  },
+};
+
+const ToggleRight = styled.div`
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  font-size: 2em;
+`;
+
+const config: StyleConfig = {
+  defaultProps: {},
+  staticProps: [],
+};
+
+export const BaseAccordion = applyTheme(styles, config);
+
+export default function Accordion(props: any) {
+  const { title, children, onToggle, defaultToggled } = props;
+
+  return (
+    <BaseAccordion onToggle={onToggle || noop} defaultToggled={defaultToggled}>
+      <BaseAccordion.Header>
+        <h2>{title}</h2>
+        <ToggleRight>
+          <BaseAccordion.ToggleOff>
+            <FaSVG>
+              <path fill="currentColor" d={Plus} />
+            </FaSVG>
+          </BaseAccordion.ToggleOff>
+          <BaseAccordion.ToggleOn>
+            <FaSVG>
+              <path fill="currentColor" d={Minus} />
+            </FaSVG>
+          </BaseAccordion.ToggleOn>
+        </ToggleRight>
+      </BaseAccordion.Header>
+      <BaseAccordion.Content>{children}</BaseAccordion.Content>
+    </BaseAccordion>
+  );
+}
