942a9752cff3d0025bc9985255aac152a324257f
Junmin Ahn <junmin@button.is>
2021-03-10 15:36:47 -0800

    feat(component-library): support gutter, justify, and align for Grid component

--- packages/bcgov-theme/stories/Grid.stories.tsx
+++ packages/bcgov-theme/stories/Grid.stories.tsx
@@ -9,11 +9,117 @@ export default {
   component: Grid,
 } as Meta;
 
+const colStyle = {
+  border: '1px solid black',
+};
+
+const getRandomInt = max => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+const Box = () => (
+  <div
+    style={{
+      border: '1px solid black',
+      height: `${getRandomInt(40) + 25}px`,
+    }}
+  >
+    Column
+  </div>
+);
+
 const Component = props => (
   <Grid {...props}>
     <Grid.Row collapse={props.collapse}>
-      <Grid.Col span={5}>Column 1</Grid.Col>
-      <Grid.Col span={15}>Column 2</Grid.Col>
+      <Grid.Col span={5}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={15}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <Grid.Row gutter={[10, 10]}>
+      <Grid.Col span={5}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={15}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <h3>Justify - Start, Align - Start</h3>
+    <Grid.Row justify="start" align="top">
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <h3>Justify - End, Align - End</h3>
+    <Grid.Row justify="end" align="bottom">
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <h3>Justify - Center, Align - Center</h3>
+    <Grid.Row justify="center" align="center">
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <h3>Justify - Space Between</h3>
+    <Grid.Row justify="space-between">
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+    </Grid.Row>
+    <h3>Justify - Space Around</h3>
+    <Grid.Row justify="space-around">
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box />
+      </Grid.Col>
     </Grid.Row>
   </Grid>
 );
