a570ada941a11deddea8cdc2145f0ea7abadacbe
Jonathan Langlois <jon@button.is>
2021-05-31 14:06:55 -0700

    test(bcgov/radiobutton): update test for radiobutton
    add to storybook, extend unit tests
    
    #221

--- packages/bcgov-theme/__tests__/RadioButton.test.tsx
+++ packages/bcgov-theme/__tests__/RadioButton.test.tsx
@@ -1,16 +1,36 @@
 import { axe, toHaveNoViolations } from 'jest-axe';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
-import RadioButton from '../src/RadioButton';
+import RadioButton, { styles } from '../src/RadioButton';
 import 'regenerator-runtime/runtime';
+import '@testing-library/jest-dom/extend-expect';
+import { changeSelectorToObject } from '../utils/test-helpers';
 
 expect.extend(toHaveNoViolations);
 
-describe('Checkbox', () => {
+describe('RadioButton', () => {
   it('Should have no accessibility violations', async () => {
     const { container } = render(<RadioButton label="radio" />);
     const results = await axe(container);
 
     expect(results).toHaveNoViolations();
+  });
+
+  it('Should apply size styles from props to container', () => {
+    render(<RadioButton id="test" size="small" label="test" />);
+    // Input is nested inside a label, inside the container
+    const radioButton = document.getElementById('test').parentElement.parentElement;
+    const stylesObject = changeSelectorToObject(styles.size.small.container);
+    expect(radioButton).toHaveStyle(stylesObject.base);
+  });
+
+  it('Should pass through end-user props', () => {
+    const handleClick = jest.fn();
+    render(<RadioButton id="test" size="small" label="test" name="my-name" onClick={handleClick} />);
+    const radioButton = document.getElementById('test');
+    expect(radioButton).toHaveAttribute('name', 'my-name');
+    fireEvent.click(radioButton);
+
+    expect(handleClick).toHaveBeenCalled();
   });
 });
