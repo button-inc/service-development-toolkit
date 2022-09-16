059d1ef85913dce4cadab6820c1d58006c444c89
Brianna Cerkiewicz <brianna@button.is>
2022-09-16 14:00:23 -0700

    chore: create button Alert story

---
+++ packages/button-theme/stories/Alert.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Alert from '../src/Alert';
+import { HtmlOnlyWrapper } from '../../../stories/helpers';
+
+export default {
+  title: 'Alert',
+  component: Alert,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+      description: 'The size of the datepicker',
+    },
+    variant: {
+      description: 'The style variant to use.',
+      control: {
+        type: 'select',
+        options: ['success', 'info', 'warning', 'danger'],
+      },
+    },
+    closable: {
+      description: 'Set to true to allow the alert to be closed by the user.',
+    },
+    content: {
+      description: 'Pass text to display in the message.',
+    },
+  },
+} as Meta;
+
+const Template: Story = args => <Alert {...args} />;
+
+const HTMLTemplate: Story = args => (
+  <>
+    <HtmlOnlyWrapper>
+      <Alert {...args} />
+    </HtmlOnlyWrapper>
+  </>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  variant: 'success',
+  size: 'medium',
+  closable: false,
+  content: 'You have been alerted.',
+};
+
+export const HTML = HTMLTemplate.bind({});
+HTML.args = {
+  variant: 'primary',
+  size: 'medium',
+  closable: false,
+  content: 'You have been alerted.',
+};
