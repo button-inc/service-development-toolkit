dab7d40ca9ae9b2b0a247efa086c23143bcce41f
Brianna Cerkiewicz <brianna@button.is>
2022-10-14 13:32:28 -0700

    chore: create button Card story

---
+++ packages/button-theme/stories/Card.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Card from '../src/Card';
+import Link from '../src/Link';
+import Button from '../src/Button';
+import { HtmlOnlyWrapper } from '../../../stories/helpers';
+
+export default {
+  title: 'Card',
+  component: Card,
+  argTypes: {
+    title: {
+      description: 'The title of the card to display in its header.',
+    },
+  },
+} as Meta;
+
+const Template: Story = args => (
+  <Card {...args}>
+    If you'd like to register online please ensure that you have the <Link href="#link1">supporting documents</Link>{' '}
+    available
+    <br /> <br />
+    <Button>Click here to register</Button>
+    <br /> <br />
+    <Link href="#link1">
+      Already registered? Click here to <strong>login</strong>
+    </Link>
+  </Card>
+);
+
+// duplication in template is to make code block show the children
+const HTMLTemplate: Story = args => (
+  <HtmlOnlyWrapper>
+    <Card {...args}>
+      If you'd like to register online please ensure that you have the <Link href="#link1">supporting documents</Link>{' '}
+      available
+      <br /> <br />
+      <Button>Click here to register</Button>
+      <br /> <br />
+      <Link href="#link1">
+        Already registered? Click here to <strong>login</strong>
+      </Link>
+    </Card>
+  </HtmlOnlyWrapper>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Online Registration',
+};
+
+export const HTML = HTMLTemplate.bind({});
+HTML.args = {
+  title: 'Online Registration',
+};
