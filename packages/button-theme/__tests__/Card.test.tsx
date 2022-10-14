04ef1a28c1b6c358b4f93dfd6bba732c45b6e195
Brianna Cerkiewicz <brianna@button.is>
2022-10-14 13:31:17 -0700

    test: test button Card component

---
+++ packages/button-theme/__tests__/Card.test.tsx
@@ -0,0 +1,16 @@
+import { axe, toHaveNoViolations } from 'jest-axe';
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import Card from '../src/Card';
+import 'regenerator-runtime/runtime';
+
+expect.extend(toHaveNoViolations);
+
+describe('Card', () => {
+  it('Should have no accessibility violations', async () => {
+    const { container } = render(<Card />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
