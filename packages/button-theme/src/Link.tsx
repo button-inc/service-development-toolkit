b554b9d9edca2d7fef4c4d604dd4d1ed05a00c35
Brianna Cerkiewicz <brianna@button.is>
2022-10-14 07:36:41 -0700

    feat: create Link component in Button theme

--- packages/button-theme/src/Link.tsx
+++ packages/button-theme/src/Link.tsx
@@ -1,42 +1,14 @@
-import React from 'react';
 import { applyTheme, StyleConfig } from '@button-inc/component-library/Link';
-
-export const sizes: {
-  [key: string]: string;
-} = {
-  small: '0.8rem',
-  medium: '1rem',
-  large: '1.2rem',
-};
-
-type Props = {
-  size?: string;
-};
-
-const StyledAnchor = styled.a`
-  font-size: ${(props: Props) => sizes[props.size || 'medium']};
-  color: #1a5a96;
-
-  &:hover {
-    text-decoration: none;
-    color: blue;
-  }
-
-  &:focus {
-    outline: 4px solid #3b99fc;
-    outline-offset: 1px;
-  }
-`;
+import styles from './styles';
 
 const config: StyleConfig = {
   defaultProps: {
-    variant: 'primary',
     size: 'medium',
   },
   breakProps: [],
   staticProps: [],
 };
 
-const Button = applyTheme(styles, config);
+const Link = applyTheme(styles, config);
 
-export default Button;
+export default Link;
