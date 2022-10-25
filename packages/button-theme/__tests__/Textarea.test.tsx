4a1f6210eb779008e0ced4bc3f73841aec6d7287
Ballard <ballard@button.is>
2022-10-25 14:57:38 -0400

    chore: added testing to textarea

--- packages/button-theme/__tests__/Textarea.test.tsx
+++ packages/button-theme/__tests__/Textarea.test.tsx
@@ -1,5 +1,5 @@
 import { axe, toHaveNoViolations } from 'jest-axe';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Textarea from '../src/Textarea';
 import '@testing-library/jest-dom/extend-expect';
@@ -13,5 +13,29 @@ describe('Textarea', () => {
     const results = await axe(container);
 
     expect(results).toHaveNoViolations();
+  });
+
+  it('Should pass through end-user props', () => {
+    const handleClick = jest.fn();
+    const handleChange = jest.fn();
+    render(<Textarea onClick={handleClick} onChange={handleChange} id="test" />);
+    const textarea = document.getElementById('test');
+    fireEvent.click(textarea);
+    fireEvent.change(textarea, { target: { value: 'test' } });
+
+    expect(handleClick).toHaveBeenCalled();
+    expect(handleChange).toHaveBeenCalled();
+  });
+
+  it('Should accept the fullWidth static prop', () => {
+    render(<Textarea id="test" fullWidth />);
+    const textarea = document.getElementById('test');
+    expect(textarea).toHaveStyle('width: 100%;');
+  });
+
+  it('Should pass on user attributes', () => {
+    render(<Textarea id="test" fullWidth rounded cols={5} />);
+    const textarea = document.getElementById('test');
+    expect(textarea).toHaveAttribute('cols', '5');
   });
 });
