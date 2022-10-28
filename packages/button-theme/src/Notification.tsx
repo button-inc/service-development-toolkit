d9006f9b25995a4ac5fa3517080e7aec9f0fbd61
Brianna Cerkiewicz <77306764+BCerki@users.noreply.github.com>
2022-10-28 14:21:10 -0700

    refactor: make Notification component use global styles object
    * style: add styles object to Notification component
    
    * refactor: make Notification component use global styles object
    
    * refactor: make Notification component use global styles object
    
    * chore: revert changes to test happo
    
    * refactor: make Notification component use global styles object
    
    * chore: remove unused variants
    
    * chore: remove unused variants
    
    * chore: remove unused styles and spread size
    
    * chore: clean up comments

--- packages/button-theme/src/Notification.tsx
+++ packages/button-theme/src/Notification.tsx
@@ -1,6 +1,8 @@
 import { applyTheme, StyleConfig } from '@button-inc/component-library/Notification';
+import styles from './styles';
 
-export const styles = {
+export const notificationStyles = {
+  ...styles,
   shared: {
     container: `
       position: relative;
@@ -28,156 +30,32 @@ export const styles = {
   },
   size: {
     small: {
+      ...styles.size.small,
       container: `
         padding: 0.8rem 0.9rem;
       `,
-      header: `
-        font-size: 1rem;
-      `,
       content: `
         font-size: 0.8rem;
-      `,
-      close: `
-        font-size: 0.8rem;
-        padding: 0.2rem;
       `,
     },
     medium: {
+      ...styles.size.medium,
       container: `
         padding: 1rem 1.1rem;
       `,
-      header: `
-        font-size: 1.2rem;
-      `,
       content: `
         font-size: 1rem;
-      `,
-      close: `
-        font-size: 1rem;
-        padding: 0.4rem;
       `,
     },
     large: {
+      ...styles.size.large,
       container: `
         padding: 1.2rem 1.3rem;
       `,
-      header: `
-        font-size: 1.4rem;
-      `,
       content: `
         font-size: 1.2rem;
       `,
-      close: `
-        font-size: 1.2rem;
-        padding: 0.6rem;
-      `,
     },
-  },
-  variant: {
-    standard: {
-      container: `
-        border: 1px solid #707070;
-        background-color: #F2F2F2;
-      `,
-      header: `
-        color: #000;
-      `,
-      content: `
-        color: #000;
-      `,
-      close: `
-        color: #000;
-        border: 1px solid #000;
-
-        &:hover {
-          background: #000;
-          color: #fff;
-        }
-      `,
-      group: `
-        color: #000;
-      `,
-    },
-    warning: {
-      container: `
-        border: 2px solid #b58108;
-        background-color: #FFF8DB;
-      `,
-      header: `
-        color: #b58108;
-      `,
-      content: `
-        color: #b58108;
-      `,
-      close: `
-        color: #b58108;
-        border: 1px solid #b58108;
-
-        &:hover {
-          background: #b58108;
-          color: #fff;
-        }
-      `,
-      group: `
-        color: #b58108;
-      `,
-    },
-    danger: {
-      container: `
-        border: 2px solid #DB2828;
-        background-color: #FEE8E6;
-      `,
-      header: `
-        color: #db2828;
-      `,
-      content: `
-        color: #db2828;
-      `,
-      close: `
-        color: #db2828;
-        border: 1px solid #db2828;
-
-        &:hover {
-          background: #db2828;
-          color: #fff;
-        }
-      `,
-      group: `
-        color: #db2828;
-      `,
-    },
-    success: {
-      container: `
-        border: 2px solid #1ebc30;
-        background-color: #E5F9E6;
-      `,
-      header: `
-        color: #1ebc30;
-      `,
-      content: `
-        color: #1ebc30;
-      `,
-      close: `
-        color: #1ebc30;
-        border: 1px solid #1ebc30;
-
-        &:hover {
-          background: #1ebc30;
-          color: #fff;
-        }
-      `,
-      group: `
-        color: #1ebc30;
-      `,
-    },
-  },
-  flex: {
-    container: `
-      display: flex;
-    `,
-    group: `
-      margin: auto 0.5rem;
-    `,
   },
 };
 
@@ -203,6 +81,6 @@ const childStyles = {
   },
 };
 
-const Notification = applyTheme(styles, config, childStyles);
+const Notification = applyTheme(notificationStyles, config, childStyles);
 
 export default Notification;
