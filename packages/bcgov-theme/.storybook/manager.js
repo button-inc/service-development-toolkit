d6dd3e8f0b4e5b2aaa58a0a8841ee7c3f13a966b
Josh Gamache <joshua@button.is>
2022-11-03 13:23:58 -0600

    fix: favicon does not display

--- packages/bcgov-theme/.storybook/manager.js
+++ packages/bcgov-theme/.storybook/manager.js
@@ -2,7 +2,13 @@
 
 import { addons } from '@storybook/addons';
 import theme from './Theme';
+import favicon from './images/favicon-32x32.png';
 
 addons.setConfig({
   theme,
 });
+
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', favicon);
+document.head.appendChild(link);
